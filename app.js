b88280494e836124d039e20d4ac5dd7d9f3545f9
Osid ALkourd <alkordosid73@gmail.com>
2023-07-12 18:38:06 +0300

    add lesson-4 (computed properties)

--- app.js
+++ app.js
@@ -15,7 +15,14 @@ const app = Vue.createApp({
    toggleFav(book){
       book.isFav  = !book.isFav 
    }
+  } , 
+  computed: {
+    filteredBooks(){
+      return this.books.filter((book) => book.isFav)
+    }
   }
+
+  
 })
 app.mount('#app')
 
